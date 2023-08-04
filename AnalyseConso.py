287421c93b370374d19ae0ad25d4196c9350b53f
Koicoubeh <141322221+Koicoubeh@users.noreply.github.com>
2023-08-04 12:18:44 +0200

    Ajout abo ZenFlex
    Nouvelle offre EDF : abonnement Zenflex

--- AnalyseConso.py
+++ AnalyseConso.py
@@ -8,6 +8,18 @@
 # Auteur : Koicoubeh
 # Contributeur du fichier CSV d'exemple et aide au débug : @MathieuMf (Twitter)
 #
+# Ce programme est sous licence CC BY-NC-SA 4.0
+# https://creativecommons.org/licenses/by-nc-sa/4.0/
+#
+#################################################################################################
+
+#################################################################################################
+#
+# v1.0 : 2023-08-03
+# - Première version fonctionnelle
+# v1.1 : 2023-08-04
+# - Ajout de l'abonnement ZenFlex
+#
 #################################################################################################
 
 import csv
@@ -23,6 +35,9 @@ from datetime import datetime, timedelta
 # Tableaux contenant les heures creuses. Si c'est pas creux... c'est plein !
 # exemple : 0,1,2,3,4,5,22,23 : HC de 0h à 6h et de 22h à 24h
 HC = [0,1,2,3,4,5,22,23]
+
+# Horaires HC/HP pour l'abonnement ZenFlex
+HCZenFlex = [0,1,2,3,4,5,6,7,14,15,16,17,20,21,22,23]
 
 # Abonnement en kVA : 6, 9 ou 12
 abonnement = 9
@@ -65,8 +80,19 @@ AboHC6kva = 12.85
 AboHP9kva = 16.55
 AboHP12kva = 19.97
 
+ZenHPEco = 0.2228
+ZenHCEco = 0.1295
+ZenHPSobriete = 0.6712
+ZenHCSobriete = 0.2228
+ZenAbo6kva = 12.62
+ZenAbo9kva = 15.99
+ZenAbo12kva = 19.27
+
 # Dictionnaire des jours bleu blanc rouge. Chaque élément est un tuple (date, couleur)
 CalBar = {}
+
+# Dictionnaire des jours eco/sobriété de l'offre ZenFlex
+CalZen = {}
 
 # Tableau des données de consommation. Chaque élément est un tuple (date_heure, consommation HC, consommation HP)
 CalConso = []
@@ -77,7 +103,7 @@ PremierMois = 0
 DernierMois = 0
 
 # Initialisation des sommes pour chacun des forfaits testés
-simulTempo, simulBase, simulHCHP = 0.0, 0.0, 0.0
+simulTempo, simulBase, simulHCHP, simulZen = 0.0, 0.0, 0.0, 0.0
 
 # Consommation en HP et HC
 ConsoHP, ConsoHC = 0.0, 0.0
@@ -101,6 +127,20 @@ with open("calBAR.csv", newline='') as csvfile:
         # et la couleur comme valeur associée
         CalBar[date_obj.strftime("%Y-%m-%d")] = ligne[1]
 
+
+with open("calZen.csv", newline='') as csvfile:
+    # Créer un objet lecteur CSV
+    lecteur_csv = csv.reader(csvfile, delimiter=';')
+    
+    # Parcourir chaque ligne du CSV
+    for ligne in lecteur_csv:
+        # Convertir la date au format "01/09/2019" en objet datetime
+        date_str = ligne[0]
+        date_obj = datetime.strptime(date_str, "%d/%m/%Y").date()
+        
+        # Utiliser la date au format "YYYY-MM-DD" comme clé dans le dictionnaire
+        # et la couleur comme valeur associée
+        CalZen[date_obj.strftime("%Y-%m-%d")] = ligne[1]
 
 
 # Ouvrir le fichier CSV de consommation en mode lecture
@@ -175,6 +215,33 @@ with open(chemin_csv, newline='', encoding='utf-8-sig') as csvfile:
             else:
                 print(f"Valeur hors calendrier TEMPO supporté pour la ligne {i} : {jour}")
 
+            # Même opération pour le forfait ZenFlex
+            if jour in CalZen:
+                couleur = CalZen[jour]
+
+                # tester en fonction de l'heure (un jour commence à 6h, pas à minuit).
+                # Si l'heure est inférieure à 6h, on considère que c'est la veille
+                if heure < "06:00":
+                    jour = (datetime.strptime(jour,"%Y-%m-%d").date() - timedelta(days=1)).strftime("%Y-%m-%d")
+
+                # On applique les tarifs en fonction de la couleur du jour et de l'HP/HC du forfait ZenFlex
+                if couleur == "BLEU":
+                    if int(heure[:2]) in HCZenFlex:                        
+                        simulZen += consommation * ZenHCEco
+                    else:
+                        simulZen += consommation * ZenHPEco
+                elif couleur == "BLANC":
+                    if int(heure[:2]) in HCZenFlex:
+                        simulZen += consommation * ZenHCEco
+                    else:
+                        simulZen += consommation * ZenHPEco
+                elif couleur == "ROUGE":
+                    if int(heure[:2]) in HCZenFlex:
+                        simulZen += consommation * ZenHCSobriete
+                    else:
+                        simulZen += consommation * ZenHPSobriete
+
+
     # Calcul du nombre de mois que recouvre le fichier CSV
     DerniereDate = datetime.fromisoformat(date_heure.replace("Z", "+00:00"))
 
@@ -190,19 +257,23 @@ if abonnement == 6:
     simulTempo += AboTempo6kva*nbMois
     simulBase += AboBleu6kva*nbMois
     simulHCHP += AboHC6kva*nbMois
+    simulZen += ZenAbo6kva*nbMois    
 elif abonnement == 9:
     simulTempo += AboTempo9kva*nbMois
     simulBase += AboBleu9kva*nbMois
     simulHCHP += AboHP9kva*nbMois
+    simulZen += ZenAbo9kva*nbMois
 elif abonnement == 12:
     simulTempo += AboTempo12kva*nbMois
     simulBase += AboBleu12kva*nbMois
     simulHCHP += AboHP12kva*nbMois
+    simulZen += ZenAbo12kva*nbMois
 
 #Arrondir à 2 décimales
 simulTempo = round(simulTempo, 2)
 simulBase = round(simulBase, 2)
 simulHCHP = round(simulHCHP, 2)
+simulZen = round(simulZen, 2)
 ConsoHC = round(ConsoHC, 2)
 ConsoHP = round(ConsoHP, 2)
 
@@ -215,4 +286,5 @@ print(f"Simulation des coûts sur {nbMois} mois, selon les 3 forfaits :")
 print(f"  Tempo = {simulTempo} €")
 print(f"  Base = {simulBase} €")
 print(f"  HCHP = {simulHCHP} €")
+print(f"  ZenFlex = {simulZen} €")
 print()
